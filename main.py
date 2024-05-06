f7fe9d36aa0ee5326d8f1551290c9c12f67a9aee
Fatolu Stephen <smjssp@gmail.com>
2024-05-06 13:22:13 +0100

    beautify code

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import PyPDF2
 import sys
 import os
 
+# Creates a merger object
 merger = PyPDF2.PdfMerger()
 
+# Iterates over the pdfs in the current directory
 for file in os.listdir(os.curdir):
+   
+    # Checks if file has a .pdf extenstion
     if file.endswith(".pdf"):
+        
         merger.append(file)
+    
+    # Combined file name
     merger.write("combinedDocs.pdf")
